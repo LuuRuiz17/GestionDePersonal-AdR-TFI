45af238de469512ca007bf4243e2acb85b906011
JulianAballay <juliancabj1210@gmail.com>
2025-11-16 00:11:22 -0300

    agrego el icono circular de la persona y ademas hago que el boton tome todo el ancho de la card

--- src/components/pages/LoginEmpleados.jsx
+++ src/components/pages/LoginEmpleados.jsx
@@ -1,49 +1,49 @@
-import { Container, Card, Button, Form, InputGroup  } from "react-bootstrap";
+import { Container, Card, Button, Form, InputGroup } from "react-bootstrap";
 
 const LoginEmpleados = () => {
   return (
     <section className="contenidoPrincipal d-flex justify-content-center align-items-center">
-      <Container>
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <div className="text-center">
-              <Card.Title>Sistema de Gestión de Personal</Card.Title>
-              <p className="text-muted">
-                Ingrese sus credenciales para acceder al sistema
-              </p>
-            </div>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Número de legajo</Form.Label>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text id="basic-addon1"><i class="bi bi-people"></i></InputGroup.Text>
-                  <Form.Control
-                    placeholder="Ej: 1234"
-                    type="text"
-                    required
-                  />
-                </InputGroup>
-              </Form.Group>
+      <Card className="cardLogin">
+        <Card.Body className="p-4">
+          <div className="text-center fs-1">
+            <i className="bi bi-person-circle text-primary"></i>
+          </div>
+          <div className="text-center">
+            <h4>Sistema de Gestión de Personal</h4>
+            <p>Ingrese sus credenciales para acceder al sistema</p>
+          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Número de legajo</Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">
+                  <i className="bi bi-people"></i>
+                </InputGroup.Text>
+                <Form.Control placeholder="Ej: 1234" type="text" required />
+              </InputGroup>
+            </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text id="basic-addon1"><i class="bi bi-lock-fill"></i></InputGroup.Text>
-                  <Form.Control
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">
+                  <i className="bi bi-lock-fill"></i>
+                </InputGroup.Text>
+                <Form.Control
                   type="password"
-                    placeholder="Ingrese su contraseña"
-                    required
-                  />
-                </InputGroup>
-              </Form.Group>
-              
-              <Button variant="primary" type="submit">
-                Iniciar Sesión
+                  placeholder="Ingrese su contraseña"
+                  required
+                />
+              </InputGroup>
+            </Form.Group>
+            <div className="d-flex justify-content-center mt-4">
+              <Button variant="primary" className="w-100" type="submit">
+                <i class="bi bi-arrow-bar-right"></i> Iniciar Sesión
               </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Container>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
     </section>
   );
 };
