8d1ca6ae6e97a4019b820849beb99057a44836ed
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-11-16 18:53:15 -0300

    Agregué un import para eliminar un error de la consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import LoginEmpleados from "./components/pages/LoginEmpleados";
 import MenuPrincipal from "./components/pages/MenuPrincipal";
 import GestionarEmpleados from "./components/pages/GestionarEmpleados";
+
 function App() {
   return (
     <>
