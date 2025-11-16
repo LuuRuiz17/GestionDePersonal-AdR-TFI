3d13bc4aa957fd1fb569347b66d4a60e552d42ee
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-11-16 18:53:51 -0300

    Eliminé el llamado a los componentes de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import GestionarEmpleados from "./components/pages/GestionarEmpleados";
 function App() {
   return (
     <>
-      {/* <MenuPrincipal /> */}
-      <GestionarEmpleados />
     </>
   );
 }
