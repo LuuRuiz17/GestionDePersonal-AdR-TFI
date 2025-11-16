16798fe241eda3e55c8c285bedcafbd1f7e23132
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-11-16 18:10:14 -0300

    Agregué los títulos y los iconos para cada acción

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginEmpleados from './components/pages/LoginEmpleados';
-import MenuPrincipal from './components/pages/MenuPrincipal';
-import GestionarEmpleados from './components/pages/GestionarEmpleados';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginEmpleados from "./components/pages/LoginEmpleados";
+import MenuPrincipal from "./components/pages/MenuPrincipal";
+import GestionarEmpleados from "./components/pages/GestionarEmpleados";
 function App() {
-
   return (
     <>
-      <MenuPrincipal></MenuPrincipal>
+      {/* <MenuPrincipal /> */}
+      <GestionarEmpleados />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/GestionarEmpleados.jsx
+++ src/components/pages/GestionarEmpleados.jsx
@@ -3,21 +3,82 @@ import { Card, Row, Col, Container } from "react-bootstrap";
 const GestionarEmpleados = () => {
   return (
     <>
-      <Container className="my-auto">
+      <Container className="my-4 px-3">
         <h1 className="titulo">Gestionar Empleados</h1>
         <h5 className="titulo">Seleccione la acción que desea realizar</h5>
-        <Row className="d-flex justify-content-center flex-nowrap mt-4">
-          <Col className="bg-dark me-2 rounded-3">
-            <h3 className="text-info">Agregar Empleado</h3>
-            <p>Registrar un nuevo empleado en el sistema</p>
+
+        <Row className="justify-content-center mt-4 g-3 flex-nowrap">
+          <Col
+            xs={12}
+            sm={4}
+            md={4}
+            lg={4}
+            className="bg-light px-3 py-5 rounded-4 cardMenu me-2"
+          >
+            <div className="my-3 text-center mb-4">
+              <i
+                className="bi bi-person-add fs-4 text-light py-3 px-4 rounded-4"
+                style={{ backgroundColor: "#459F46" }}
+              ></i>
+            </div>
+            <h3 className="titulo text-center">Agregar Empleado</h3>
+            <p className="text-center">
+              Registrar un nuevo empleado en el sistema
+            </p>
           </Col>
-          <Col className="bg-dark me-2 rounded-3">
-            <h3 className="text-info titulo">Editar Empleado</h3>
-            <p>Modificar información de empleados existentes</p>
+
+          <Col
+            xs={12}
+            sm={4}
+            md={4}
+            lg={4}
+            className="bg-light px-3 py-5 rounded-4 cardMenu me-2"
+          >
+            <div className="my-3 text-center mb-4">
+              <i
+                className="bi bi-person-gear fs-4 text-light py-3 px-4 rounded-4"
+                style={{ backgroundColor: "#1D88E5" }}
+              ></i>
+            </div>
+            <h3 className="titulo text-center">Editar Empleado</h3>
+            <p className="text-center">
+              Modificar información de empleados existentes
+            </p>
           </Col>
-          <Col className="bg-dark rounded-3">
-            <h3 className="text-info">Eliminar Empleado</h3>
-            <p>Dar de baja empleados del sistema</p>
+
+          <Col
+            xs={12}
+            sm={4}
+            md={4}
+            lg={4}
+            className="bg-light px-3 py-5 rounded-4 cardMenu"
+          >
+            <div className="my-3 text-center mb-4">
+              <i
+                className="bi bi-person-dash fs-4 text-light py-3 px-4 rounded-4"
+                style={{ backgroundColor: "#E43A33" }}
+              ></i>
+            </div>
+            <h3 className="titulo text-center">Eliminar Empleado</h3>
+            <p className="text-center">Dar de baja empleados del sistema</p>
+          </Col>
+        </Row>
+
+        <Row className="mb-3 p-4 bg-light rounded-4 cardMenu mt-4">
+          <Col>
+            <h4 className="mb-4">Información Importante</h4>
+            <p className="ms-4">
+              <b>Agregar:</b> Complete todos los datos requeridos del nuevo
+              empleador
+            </p>
+            <p className="ms-4">
+              <b>Editar:</b> Busque el empleado por legajo o nombre para
+              modificar sus datos
+            </p>
+            <p className="ms-4">
+              <b>Eliminar:</b> La eliminación es permanente. Asegúrese de
+              confirmar la acción
+            </p>
           </Col>
         </Row>
       </Container>
