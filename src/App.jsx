25498f497737526d4c065cb14377869fc1679827
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-11-16 02:37:39 -0300

    Creé una grilla que muestra las acciones que se pueden realizar en el sistema

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginEmpleados from './components/pages/LoginEmpleados';
+import MenuPrincipal from './components/pages/MenuPrincipal';
 function App() {
 
   return (
     <>
-      <LoginEmpleados></LoginEmpleados>
+      {/* <LoginEmpleados></LoginEmpleados> */}
+      <MenuPrincipal></MenuPrincipal>
     </>
   )
 }
